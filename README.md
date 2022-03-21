e872b98605e3992678b41aa79011ae3edccd0b25
e-yang08 <95173202+e-yang08@users.noreply.github.com>
2022-03-21 21:23:12 +0100

    Upload lab 2 edition

--- README.md
+++ README.md
@@ -1,5 +1,40 @@
 ## Flashcard App
 
+## Lab 2
+
+### App Description
+This application allows users to interactively use flashcard by...
+- Flipping the question and answer cards
+- Choosing an answer from multiple choices
+- Showing the correct / wrong answer(s) by color
+- Hiding and showing multiple choices
+- Resetting all the changes
+- Adding new flashcard with correponding choices
+- Noticing errors when the fields are blank
+
+### App Walk-through
+Below is a gif that animated app walk-through.
+
+<img src="https://i.imgur.com/gw3Bgpw.gif" width=200><br>
+
+## Required
+- [x] User can click on a ‘+’ button that takes the user to new ‘Add Card Screen’
+- [x] The 'Add Card Screen' has a cancel button to take the user back to the main screen
+- [x] User can enter and submit a Question and Answer through the 'Add Card Screen'
+- [x] User will see the card they just created on the main screen when they press the 'Save Button'
+- [x] Push your progress to GitHub!
+
+## Optional
+- [x] User can edit a card
+- [x] An error message is shown if the user doesn't enter both a Question and an Answer
+- [x] A notification is shown if the card was created successfully
+- [x] User can also enter multiple choice answers when creating a card
+- [x] App is further styled and customized!
+   - [x] Multiple choices are randomly ordered when card is created
+   - [x] Add / Edit card field displays floating label feedback
+   - [x] Add / Edit card field shows error message when the field is blank 
+
+
 ## Lab 1
 
 ### App Description
@@ -28,3 +63,4 @@ Below is a gif that animated app walk-through.
 - [x] Add selectable multiple choice answers beneath the card
    - [x] Change the background color of the multiple choice answers when clicked to indicate whether the question was answered correctly
 - [x] Further customize and style the card
+
--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,84 +1,127 @@
 package com.example.flashcard;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
     boolean answerChoicesVisible = true;
+
+    // initialized the index of correct / wrong answer choices
+
+    int wrongChoice1IndexInt = new Integer(1);
+    int correctChoiceIndexInt = new Integer(2);
+    int wrongChoice2IndexInt = new Integer(3);
+    int addCARD_REQUEST_CODE = 100;
+    int editCARD_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView flashcardQ = findViewById(R.id.flashcard_question_textview);
-        TextView flashcardA = findViewById(R.id.flashcard_answer_textview);
-        TextView choice1 = findViewById(R.id.answerChoice1_textview);
-        TextView choice2 = findViewById(R.id.answerChoice2_textview);
-        TextView choice3 = findViewById(R.id.answerChoice3_textview);
-        ImageView toggle = findViewById(R.id.toggle_choices_visibility_imageview);
+
+//        System.out.println("before: correct"+correctChoiceIndexInt+"wrong1"+wrongChoice1IndexInt+"wrong2"+wrongChoice2IndexInt);
+
+        TextView flashcardQTextView = findViewById(R.id.flashcard_question_textview);
+        TextView flashcardATextView = findViewById(R.id.flashcard_answer_textview);
+        TextView answerChoice1TextView = findViewById(R.id.answerChoice1_textview);
+        TextView answerChoice2TextView = findViewById(R.id.answerChoice2_textview);
+        TextView answerChoice3TextView = findViewById(R.id.answerChoice3_textview);
+
+        ImageView toggleImageView = findViewById(R.id.toggle_choices_visibility_imageview);
 
         // tap on question card to show the answer of flashcard
-        flashcardQ.setOnClickListener(new View.OnClickListener() {
+        flashcardQTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                flashcardQ.setVisibility(View.INVISIBLE);
-                flashcardA.setVisibility(View.VISIBLE);
+                flashcardQTextView.setVisibility(View.INVISIBLE);
+                flashcardATextView.setVisibility(View.VISIBLE);
             }
         });
 
         // tap on answer card to go back to the question of flashcard
-        flashcardA.setOnClickListener(new View.OnClickListener() {
+        flashcardATextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                flashcardA.setVisibility(View.INVISIBLE);
-                flashcardQ.setVisibility(View.VISIBLE);
+                flashcardATextView.setVisibility(View.INVISIBLE);
+                flashcardQTextView.setVisibility(View.VISIBLE);
             }
         });
 
-        // tap the wrong answer and check the correct answer
-        choice1.setOnClickListener(new View.OnClickListener() {
+
+        // light up the answer in green when user tap the correct answer
+        // but in red otherwise -- the correct answer is shown in green background
+        answerChoice1TextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                choice1.setBackgroundColor(getResources().getColor(R.color.sheer_red, null));
-                choice2.setBackgroundColor(getResources().getColor(R.color.green, null));
+                if (correctChoiceIndexInt == 1){
+                    answerChoice1TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                } else if (correctChoiceIndexInt == 2){
+                    answerChoice1TextView.setBackgroundColor(getResources().getColor(R.color.sheer_red, null));
+                    answerChoice2TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                } else {
+                    answerChoice1TextView.setBackgroundColor(getResources().getColor(R.color.sheer_red, null));
+                    answerChoice3TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                }
             }
         });
 
-        // tap the correct answer
-        choice2.setOnClickListener(new View.OnClickListener() {
+        answerChoice2TextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                choice2.setBackgroundColor(getResources().getColor(R.color.green, null));
+                if (correctChoiceIndexInt == 1){
+                    answerChoice1TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                    answerChoice2TextView.setBackgroundColor(getResources().getColor(R.color.sheer_red, null));
+                } else if (correctChoiceIndexInt == 2){
+                    answerChoice2TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                } else {
+                    answerChoice2TextView.setBackgroundColor(getResources().getColor(R.color.sheer_red, null));
+                    answerChoice3TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                }
             }
         });
 
-        // tap the wrong answer and check the correct answer
-        choice3.setOnClickListener(new View.OnClickListener() {
+        answerChoice3TextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                choice2.setBackgroundColor(getResources().getColor(R.color.green, null));
-                choice3.setBackgroundColor(getResources().getColor(R.color.sheer_red, null));
+                if (correctChoiceIndexInt == 1){
+                    answerChoice1TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                    answerChoice3TextView.setBackgroundColor(getResources().getColor(R.color.sheer_red, null));
+                } else if (correctChoiceIndexInt == 2){
+                    answerChoice2TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                    answerChoice3TextView.setBackgroundColor(getResources().getColor(R.color.sheer_red, null));
+                } else {
+                    answerChoice3TextView.setBackgroundColor(getResources().getColor(R.color.green, null));
+                }
             }
         });
 
         // tap on toggle button to show or hide answer choices
-        toggle.setOnClickListener(new View.OnClickListener() {
+        toggleImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (answerChoicesVisible) {
-                    choice1.setVisibility(View.INVISIBLE);
-                    choice2.setVisibility(View.INVISIBLE);
-                    choice3.setVisibility(View.INVISIBLE);
-                    toggle.setImageResource(R.drawable.show_view);
+                    answerChoice1TextView.setVisibility(View.INVISIBLE);
+                    answerChoice2TextView.setVisibility(View.INVISIBLE);
+                    answerChoice3TextView.setVisibility(View.INVISIBLE);
+                    toggleImageView.setImageResource(R.drawable.show_view);
                     answerChoicesVisible = false;
                 } else {
-                    choice1.setVisibility(View.VISIBLE);
-                    choice2.setVisibility(View.VISIBLE);
-                    choice3.setVisibility(View.VISIBLE);
-                    toggle.setImageResource(R.drawable.hide_view);
+                    answerChoice1TextView.setVisibility(View.VISIBLE);
+                    answerChoice2TextView.setVisibility(View.VISIBLE);
+                    answerChoice3TextView.setVisibility(View.VISIBLE);
+                    toggleImageView.setImageResource(R.drawable.hide_view);
                     answerChoicesVisible = true;
                 }
             }
@@ -90,20 +133,143 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //set multiple choices' visibility
                 answerChoicesVisible = true;
-                choice1.setVisibility(View.VISIBLE);
-                choice2.setVisibility(View.VISIBLE);
-                choice3.setVisibility(View.VISIBLE);
-                toggle.setImageResource(R.drawable.hide_view);
+                answerChoice1TextView.setVisibility(View.VISIBLE);
+                answerChoice2TextView.setVisibility(View.VISIBLE);
+                answerChoice3TextView.setVisibility(View.VISIBLE);
+                toggleImageView.setImageResource(R.drawable.hide_view);
 
                 // set Question and answer cards' visibility
-                flashcardA.setVisibility(View.INVISIBLE);
-                flashcardQ.setVisibility(View.VISIBLE);
+                flashcardATextView.setVisibility(View.INVISIBLE);
+                flashcardQTextView.setVisibility(View.VISIBLE);
 
                 //set multiple choices' color
-                choice1.setBackgroundColor(getResources().getColor(R.color.white, null));
-                choice2.setBackgroundColor(getResources().getColor(R.color.white, null));
-                choice3.setBackgroundColor(getResources().getColor(R.color.white, null));
+                answerChoice1TextView.setBackgroundColor(getResources().getColor(R.color.white, null));
+                answerChoice2TextView.setBackgroundColor(getResources().getColor(R.color.white, null));
+                answerChoice3TextView.setBackgroundColor(getResources().getColor(R.color.white, null));
             }
         });
+
+        ImageView addQuestionImageView = findViewById(R.id.add_button_imageview);
+//        ImageView nextQuestionImageView = findViewById(R.id.next_button_imageview);
+        ImageView editQuestionImageView = findViewById(R.id.edit_button_imageview);
+
+        // tap plus button to move to a new page
+        addQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                startActivityForResult(intent, addCARD_REQUEST_CODE);
+            }
+        });
+
+        // tap edit button to move to edit page
+        editQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                intent.putExtra("question",flashcardQTextView.getText().toString().substring(3));
+                intent.putExtra("correctAnswer",flashcardATextView.getText().toString().substring(3));
+
+                // based on answer choice's index, receive different input
+                if (correctChoiceIndexInt == 1) {
+                    intent.putExtra("wrongAnswer1", answerChoice2TextView.getText().toString().substring(3));
+                    intent.putExtra("wrongAnswer2", answerChoice3TextView.getText().toString().substring(3));
+                } else if (correctChoiceIndexInt == 2){
+                    intent.putExtra("wrongAnswer1",answerChoice1TextView.getText().toString().substring(3));
+                    intent.putExtra("wrongAnswer2",answerChoice3TextView.getText().toString().substring(3));
+                } else {
+                    intent.putExtra("wrongAnswer1",answerChoice1TextView.getText().toString().substring(3));
+                    intent.putExtra("wrongAnswer2",answerChoice2TextView.getText().toString().substring(3));
+                }
+                startActivityForResult(intent, editCARD_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        TextView flashcardQTextView = findViewById(R.id.flashcard_question_textview);
+        TextView flashcardATextView = findViewById(R.id.flashcard_answer_textview);
+        TextView answerChoice1TextView = findViewById(R.id.answerChoice1_textview);
+        TextView answerChoice2TextView = findViewById(R.id.answerChoice2_textview);
+        TextView answerChoice3TextView = findViewById(R.id.answerChoice3_textview);
+
+        // randomly shuffle a integer from [1,2,3]
+        List<Integer> choiceList = Arrays.asList(1,2,3);
+        Collections.shuffle(choiceList);
+
+        // assign answer choices
+        correctChoiceIndexInt = choiceList.get(0);
+        wrongChoice1IndexInt = choiceList.get(1);
+        wrongChoice2IndexInt = choiceList.get(2);
+
+//        System.out.println("post: correct"+correctChoiceIndexInt+"wrong1"+wrongChoice1IndexInt+"wrong2"+wrongChoice2IndexInt);
+
+
+        if (data != null) {
+            // key has to match with the one from addcardactivity
+            String questionString = data.getExtras().getString("question");
+            String correctAnswerString = data.getExtras().getString("correctAnswer");
+            String wrongAnswer1String = data.getExtras().getString("wrongAnswer1");
+            String wrongAnswer2String = data.getExtras().getString("wrongAnswer2");
+
+            flashcardQTextView.setText("Q. " + questionString);
+            flashcardATextView.setText("A. " + correctAnswerString);
+
+            // based on the shuffled result, set choices in corresponding textview
+            if (correctChoiceIndexInt == 1){
+                answerChoice1TextView.setText("1. " + correctAnswerString);
+                if (wrongChoice1IndexInt == 2){
+                    answerChoice2TextView.setText("2. " + wrongAnswer1String);
+                    answerChoice3TextView.setText("3. " + wrongAnswer2String);
+                } else {
+                    answerChoice2TextView.setText("2. " + wrongAnswer2String);
+                    answerChoice3TextView.setText("3. " + wrongAnswer1String);
+                }
+            } else if (correctChoiceIndexInt == 2){
+                answerChoice2TextView.setText("2. " + correctAnswerString);
+                if (wrongChoice1IndexInt == 1){
+                    answerChoice1TextView.setText("1. " + wrongAnswer1String);
+                    answerChoice3TextView.setText("3. " + wrongAnswer2String);
+                } else {
+                    answerChoice1TextView.setText("1. " + wrongAnswer2String);
+                    answerChoice3TextView.setText("3. " + wrongAnswer1String);
+                }
+            } else {
+                answerChoice3TextView.setText("3. " + correctAnswerString);
+                if (wrongChoice1IndexInt == 1) {
+                    answerChoice1TextView.setText("1. " + wrongAnswer1String);
+                    answerChoice2TextView.setText("2. " + wrongAnswer2String);
+                } else {
+                    answerChoice1TextView.setText("1. " + wrongAnswer2String);
+                    answerChoice2TextView.setText("2. " + wrongAnswer1String);
+                }
+            }
+        }
+
+        // when resultCode == RESULT_OK
+        // set the question and answer passed from main activity
+        if (requestCode == addCARD_REQUEST_CODE) {
+//            flashcardDatabase.insertCard(new Flashcard(questionString, answerString));
+//            allFlashcards = flashcardDatabase.getAllCards(); // fetch data from updated flashcards
+
+            // show a snackbar message
+            Snackbar.make(flashcardQTextView,
+                    "Card successfully created!",
+                    Snackbar.LENGTH_SHORT)
+                    .show();
+
+        } else if (requestCode == editCARD_REQUEST_CODE){
+            // show a snackbar message
+            Snackbar.make(flashcardQTextView,
+                    "Card successfully! edited",
+                    Snackbar.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
